47cecc1f66165604e79bfce5c84edf7d2f081941
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-20 00:46:37 +0530

    Update imageprocessing.py

--- imageprocessing.py
+++ imageprocessing.py
@@ -1,12 +1,21 @@
-from PIL import Image,ImageDraw, ImageFont                                 
+from PIL import Image,ImageDraw, ImageFont,ImageEnhance                               
 img = Image.open("pic.jpg")                                                 #open it
 img.show()                                                                  #show the image
-img.save("original.jpeg")                                                   #name the image
+img.save("original.jpeg")
+
+#greyscale
 Pixels = list(map(lambda x: int((x[0] + x[1] + x[2])/3), list(img.getdata()))) #average the Red,Green,Blue Value
 GreyscaleImg1 = Image.new("L", (img.size) )                               #change the image
 GreyscaleImg1.putdata(Pixels)                                     #Copies pixel list to this image
 GreyscaleImg1.show()
 GreyscaleImg1.save("greyscale.jpeg")
+
+#color contrast
+img2=ImageEnhance.Contrast(img)
+img2.enhance(1.5).show()
+img.save("contrast.jpeg")
+
+#resize the image
 print("width*height", img.width, "*", img.height)
 w=img.width
 h=img.height
@@ -19,3 +28,9 @@ if w>=800 and h>=500:
     draw.text((0, 0), "Thulasi PROJECT",(0,0,0), font=font)
     img1 = img.resize((new_wid,new_heig),Image.ANTIALIAS).show()
 img.save("resize.jpg")
+#decrease color  image contrast
+img2=ImageEnhance.Color(img)
+img2.enhance(0.5).show()
+img.save("color.jpeg")
+    
+   
